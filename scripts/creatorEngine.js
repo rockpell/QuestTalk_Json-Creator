650a182e22de0146428b298e31e93ab612041a33
rockpell <aythffk@gmail.com>
2015-05-14 17:15:03 +0900

    스크롤 문제를 제외하고 완성

--- scripts/creatorEngine.js
+++ scripts/creatorEngine.js
@@ -4,49 +4,57 @@ var player = {};
 var system = {};
 
 var nowRoom, nowScene;
+var modifyIndex;
 
 function createRoom(name){
 	room["room_name"] = name;
-	// room["start_scene"] = "scene1";
-	// room["scene" + 1] = createScene();
 }
 
 function createScene(name){
 	var scene = [];
 	room[name] = scene;
 
-	// scene.push(createMessage("라이벌", "아침", 0.5));
-	// scene.push(createMessage("라이벌", "점심", 0.5));
-	// scene.push(createMessage("라이벌", "저녁", 0.5));
-
-	// return scene;
 }
 
-function createMessage(name, text, delay){
+function createMessage(name, text, delay, actionList){
 	var message = {};
 	message["sender"] = name;
 	message["message"] = text;
 	message["delay"] = delay;
+
+	if(actionList.length != 0)
+		message["add_action_list"] = actionList;
+
+	// if(actionTarget != ""){
+	// 	var actionList = [];
+	// 	actionList.push();
+	// 	message["add_action_list"] = actionList;
+	// }
 
 	room[nowScene].push(message);
 }
 
-function insertMessage(index, name, text, delay){
+function insertMessage(index, name, text, delay, actionList){
 	var message = {};
 	message["sender"] = name;
 	message["message"] = text;
 	message["delay"] = delay;
+	
+	if(actionList.length != 0)
+		message["add_action_list"] = actionList;
+
+	// if(actionTarget != ""){
+	// 	var actionList = [];
+	// 	actionList.push();
+	// 	message["add_action_list"] = actionList;
+	// }
 
 	room[nowScene].splice(index, 0, message);
 }
 
-function modifyMessage(){
-
-}
-
 function showMessage(tbLast, mtext){
 
-	var addTag = "<tr><td><div class=\"col-xs-2\"><button class=\"btn btn-default up-button\">▲</button><button class=\"btn btn-default\">modify</button></div><div class=\"col-xs-9\"><button value=\""+mtext+"\" class=\"btn btn-default center-block data-class\">"+mtext+"</button></div><div class=\"col-xs-1\"><button class=\"btn btn-default message-delete\">Delete</button></div></td></tr>";
+	var addTag = "<tr><td><div class=\"col-xs-2\"><button class=\"btn btn-default up-button\">▲</button><button class=\"btn btn-default modify-class\">modify</button></div><div class=\"col-xs-9\"><button value=\""+mtext+"\" class=\"btn btn-default center-block data-class\">"+mtext+"</button></div><div class=\"col-xs-1\"><button class=\"btn btn-default message-delete\">Delete</button></div></td></tr>";
 
 	tbLast.before(addTag);
 }
@@ -60,14 +68,28 @@ function showAvailable(tbLast, insert){
 		return false;
 	}
 
+	var $alist = $('#message-form').find('.panel');
+	var actionList = [];
+	// var atarget = $alist.find('.action-target').html();
+	// var tname = $alist.find('.action-name').html();
+
+	// console.log("target : " + atarget + " name : " +tname);
+
+	for(var it = 0; it < $alist.size(); it++){
+		var action = {};
+		action["name"] = $($alist[it]).find('.action-name').html();
+		action["target"] = $($alist[it]).find('.action-target').html();
+		actionList.push(action);
+	}
+
 	showMessage(tbLast, $imessage.val());
 	
 
 	if(insert == undefined){
-		createMessage($isender.val(), $imessage.val(), $idelay.val());
+		createMessage($isender.val(), $imessage.val(), $idelay.val(), actionList);
 	} else {
 		var $tb = $("#message-list>tbody");
-		insertMessage(tbLast.index()-1, $isender.val(), $imessage.val(), $idelay.val());
+		insertMessage(tbLast.index()-1, $isender.val(), $imessage.val(), $idelay.val(), actionList);
 	}
 
 	$imessage.val('');
@@ -84,6 +106,15 @@ function screenClear(name){
 		}
 	});
 
+}
+
+function actionTargetClaer(){
+	var $removeList = $('#message-form').find('.panel');
+	var rmsize = $removeList.size();
+
+	for(var rm = 0; rm < rmsize; rm++){
+		$removeList[rm].remove();
+	}
 }
 
 $(document).ready(function(){
@@ -106,7 +137,7 @@ $(document).ready(function(){
 		var $roomNameInput = $("#input-room-name");
 
 		if($roomNameInput.val() != ''){
-			$tbLast.before("<tr><td><button class=\"btn btn-default home-room data-class\" value=\""+$roomNameInput.val()+"\">" + $roomNameInput.val() + '</button><button class=\"btn btn-default pull-right\">Delete</button></td>');
+			$tbLast.before("<tr><td><button class=\"btn btn-default home-room data-class\" value=\""+$roomNameInput.val()+"\">" + $roomNameInput.val() + '</button><button value=\"room\" class=\"btn btn-default pull-right data-delete\">Delete</button></td>');
 			
 			createRoom($roomNameInput.val());
 
@@ -128,7 +159,7 @@ $(document).ready(function(){
 		var $sceneNameInput = $("#input-scene-name");
 
 		if($sceneNameInput.val() != ''){
-			$tbLast.before("<tr><td><button class=\"btn btn-default room-scene data-class\" value=\""+$sceneNameInput.val()+"\">" + $sceneNameInput.val() + "</button><button class=\"btn btn-default determine-button\">★</button><button class=\"btn btn-default pull-right\">Delete</button></td>");
+			$tbLast.before("<tr><td><button class=\"btn btn-default room-scene data-class\" value=\""+$sceneNameInput.val()+"\">" + $sceneNameInput.val() + "</button><button class=\"btn btn-default determine-button\">★</button><button value=\"scene\" class=\"btn btn-default pull-right data-delete\">Delete</button></td>");
 
 			createScene($sceneNameInput.val());
 
@@ -148,8 +179,30 @@ $(document).ready(function(){
 			showAvailable($primaryButton, 'on');
 		}
 
+		$('#action-name').val('');
+
+		actionTargetClaer();
+
 		var json = JSON.stringify(room);
 		console.log(json);
+	});
+
+	$('#apply-message').click(function(){
+		$('#create-message').show();
+		$('#apply-message').hide();
+
+		var $btndata = $($('#message-list>tbody').children()[modifyIndex]).find('.data-class');
+
+		room[nowScene][modifyIndex].sender = $('#input-sender').val();
+		room[nowScene][modifyIndex].message = $('#input-message').val();
+		room[nowScene][modifyIndex].delay = $('#input-delay').val();
+
+		$btndata.val($('#input-message').val());
+		$btndata.html($('#input-message').val());
+
+		$('#input-message').val('');
+
+		actionTargetClaer();
 	});
 
 	$('#add-message').click(function(){
@@ -162,9 +215,31 @@ $(document).ready(function(){
 			$(this)
 				.removeClass('btn-default')
 				.addClass('btn-primary');
+
+			$('#create-message').show();
+			$('#apply-message').hide();
+
+			$('#input-message').val('');
+			$('#action-name').val('');
 		}
 	});
 
+});
+
+$(document).on('click', 'button.data-delete', function(){
+	if($(this).val() == "home"){
+		room = {};
+	} else {
+		var rtext = $(this).closest('tr').find('.data-class').val();
+		
+		$.each(room, function(key, value){
+			if(key == rtext){
+				room[key] = undefined;
+			}
+		});
+	}
+
+	$(this).closest('tr').remove();
 });
 
 $(document).on('click', 'button.message-delete', function(){
@@ -173,7 +248,6 @@ $(document).on('click', 'button.message-delete', function(){
 		if(room[nowScene][ms].message == $(this).closest('tr').find('.data-class').val()){
 			room[nowScene].splice(ms, 1);
 		}
-		
 	}
 
 	$(this).closest('tr').remove();
@@ -189,6 +263,36 @@ $(document).on('click', 'button.up-button', function(){
 		$(this)
 			.removeClass('btn-default')
 			.addClass('btn-primary');
+
+		$('#create-message').show();
+		$('#apply-message').hide();
+
+		$('#input-message').val('');
+		$('#action-name').val('');
+	}
+});
+
+$(document).on('click', 'button.modify-class', function(){
+	var mtext = $(this).closest('tr');
+	modifyIndex = mtext.index();
+
+	$('#input-sender').val(room[nowScene][mtext.index()].sender);
+	$('#input-message').val(room[nowScene][mtext.index()].message);
+	$('#input-delay').val(room[nowScene][mtext.index()].delay);
+
+	$('#create-message').hide();
+	$('#apply-message').show();
+
+	actionTargetClaer();
+
+	for(var al in room[nowScene][mtext.index()].add_action_list){
+
+		var atarget = room[nowScene][mtext.index()].add_action_list[al].target;
+		var tname = room[nowScene][mtext.index()].add_action_list[al].name;
+
+		var panel = "<div class=\"panel panel-default\"><div class=\"panel-body\"><span class=\"action-target\">"+atarget+"</span> : <span class=\"action-name\">"+tname+"</span><button type=\"button\" class=\"btn btn-default pull-right action-delete\">Delete</button></div></div>";
+
+		$('#add-action').closest('.input-group').after(panel);
 	}
 });
 
@@ -220,9 +324,9 @@ $(document).on('click', 'button.home-room', function(){
 
 	$.each(room, function(key, value){
 		if(key != "room_name" && key != "start_scene"){
-			$tbLast.before("<tr><td><button class=\"btn btn-default room-scene\" value=\""+key+"\">" + key + '</button></td>');
+			$tbLast.before("<tr><td><button class=\"btn btn-default room-scene data-class\" value=\""+key+"\">" + key + "</button><button class=\"btn btn-default determine-button\">★</button><button value=\"scene\" class=\"btn btn-default pull-right data-delete\">Delete</button></td>");
 		}
-	})
+	});
 
 });
 
@@ -243,6 +347,12 @@ $(document).on('click', 'button.room-scene', function(){
 		showMessage($tbLast, room[nowScene][ms].message);
 	}
 
+	$.each(room, function(key, value){
+		if(key != "room_name" && key != "start_scene"){
+			$('#action-menu').append("<li class=\"target-class btn\">"+key+"</li>");
+		}
+	});
+	
 });
 
 $(document).on('click', '#index1', function(){
@@ -254,6 +364,9 @@ $(document).on('click', '#index1', function(){
 
 		screenClear("scene");
 
+		$('#create-message').show();
+		$('#apply-message').hide();
+
 	} else if($('#frame-index').children().size() == 3){
 		$('#message-list').addClass('hide-class');
 		$('#room-list').removeClass('hide-class');
@@ -263,6 +376,9 @@ $(document).on('click', '#index1', function(){
 
 		screenClear("scene");
 		screenClear("message");
+
+		$('#create-message').show();
+		$('#apply-message').hide();
 	}
 });
 
@@ -274,5 +390,26 @@ $(document).on('click', '#index2', function(){
 		$('#frame2').hide();
 
 		screenClear("message");
+
+		$('#create-message').show();
+		$('#apply-message').hide();
 	}
+});
+
+$(document).on('click', 'li.target-class', function(){
+	var atext = $(this).html();
+	var ntext = $('#action-name').val();
+	var panel = "<div class=\"panel panel-default\"><div class=\"panel-body\"><span class=\"action-target\">"+atext+"</span> : <span class=\"action-name\">"+ntext+"</span><button type=\"button\" class=\"btn btn-default pull-right action-delete\">Delete</button></div></div>";
+
+	if(ntext == '' || ntext == undefined){
+		return;
+	}
+
+	$(this).closest('.input-group').after(panel);
+
+	$('#action-name').val('');
+});
+
+$(document).on('click', 'button.action-delete', function(){
+	$(this).parent().parent().remove();
 });
